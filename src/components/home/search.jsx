cc5e42f227baaf4511f1e0c141ed3a54252b5ceb
AndDev741 <anddesenvolved@gmail.com>
2024-12-05 11:38:18 -0300

    feat: search input implementation

---
+++ src/components/home/search.jsx
@@ -0,0 +1,16 @@
+import searchIcon from "../../assets/search.svg";
+
+function Search(){
+    return(
+        <div className="flex justify-between border-solid border-[2px] border-mainColor w-[80vw] h-[30px] rounded-[12px]">
+            <input type="text"
+                className="ml-2 outline-none w-[100%]"
+            />
+            <img src={searchIcon} 
+            className="mr-2"
+            />
+        </div>
+    )
+}
+
+export default Search;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/home/header";
+import Search from "../components/home/search";
 
 function Home(){
     return(
         <>
             <Header/>
+            <div className="flex items-center justify-center">
+                <Search />
+            </div>
         </>
     )
 }
