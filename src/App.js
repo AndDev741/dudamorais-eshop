bae6c800c980a08ad84346147a8b1b851e07dd34
AndDev741 <anddesenvolved@gmail.com>
2024-12-05 09:59:14 -0300

    feat: App configuration, tailwindcss configuration

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-mainFont">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js, jsx}"
+  ],
+  theme: {
+    screens: {
+      xs: "200px",
+      sm: "400px",
+      md: "700px",
+      lg: "1100px"
+    },
+    extend: {
+      colors: {
+        mainColor: "#B56C62",
+        ligthBrown: "#E0C2BE"
+      },
+      fontFamily: {
+        mainFont: "Inter, sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
